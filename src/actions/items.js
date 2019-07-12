c30feff4fe8b946023400323a6c7067c2b3ef0d7
ashish_pathak789 <ashish.pathak@infoprolearning.com>
2019-07-12 14:52:55 +0530

    second commit

--- src/actions/items.js
+++ src/actions/items.js
@@ -11,10 +11,7 @@ export function fetchLogin(values){
   ];
 
   return (dispatch) => {
-    /*
-     * TODO Prepare responses as they come back
-     * As opposed to in one go when all promises have returned as is current
-     */
+    
    // dispatch(updateSrcString(searchStr));
    // dispatch(requestItems(searchStr));
     return Promise.all(endpoints.map(url =>
@@ -115,10 +112,7 @@ function fetchAllItems(searchStr) {
   ];
 
   return (dispatch) => {
-    /*
-     * TODO Prepare responses as they come back
-     * As opposed to in one go when all promises have returned as is current
-     */
+   
     dispatch(updateSrcString(searchStr));
     dispatch(requestItems(searchStr));
     return Promise.all(endpoints.map(url =>
